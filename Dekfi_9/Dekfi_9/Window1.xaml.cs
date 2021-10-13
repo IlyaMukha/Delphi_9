d9f713f05412e0c84c68d8a75f304b0432e5a161
Ilya Mukha <48717147+IlyaMukha@users.noreply.github.com>
2021-10-13 18:30:08 +0300

    Add files via upload

---
+++ Dekfi_9/Dekfi_9/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dekfi_9
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void SetColor(Color text)
+        {
+            this.Background = new SolidColorBrush(text);
+        }
+    }
+}
